98727c97ab11af6ffa9718531f2d053f07bdf5e9
stabjana <garschtubald@gmail.com>
2025-11-22 21:36:04 +0200

    remove standard files, add layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+const _layout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
 }
+
+export default _layout
+
+const styles = StyleSheet.create({})
