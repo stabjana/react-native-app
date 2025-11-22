b51feebeebfee0178ef186d1e026dd147359b92f
stabjana <garschtubald@gmail.com>
2025-11-22 21:43:17 +0200

    add index and profile view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const _layout = () => {
+const RootLayout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
+    <View style={styles.container}>
+      <Text style={styles.headline}>All my contacts</Text>
     </View>
   )
 }
 
-export default _layout
+export default RootLayout
 
-const styles = StyleSheet.create({})
+// text style props siehe doku
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#9bc6ffff',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headline:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+    }
+})
