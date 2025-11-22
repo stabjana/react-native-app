8811bb01051f2692848b30f7f43513ef08855990
stabjana <garschtubald@gmail.com>
2025-11-22 21:47:19 +0200

    add routing and link

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,14 @@
+import { Stack } from 'expo-router'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.headline}>All my contacts</Text>
-    </View>
+<Stack> 
+    <Stack.Screen 
+      name="index" 
+      ></Stack.Screen>
+</Stack>
   )
 }
 
 export default RootLayout
-
-// text style props siehe doku
-const styles = StyleSheet.create({
-    container:{
-        backgroundColor: '#9bc6ffff',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headline:{
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#333',
-    }
-})
---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headline}>All my contacts</Text>
+    </View>
+  )
+}
+
+export default Index
+
+const styles = StyleSheet.create({
+        container:{
+        backgroundColor: '#9bc6ffff',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headline:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+    }
+})
